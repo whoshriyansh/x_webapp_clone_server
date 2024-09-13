52a9f9d21aeeaf6fdfcf76551cdd9b43b8258eed
whoshriyansh <shriyanshlohia0@gmail.com>
2024-09-13 11:58:07 +0530

    Changed the Token generation files

--- Server.js
+++ Server.js
@@ -23,9 +23,8 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 const __dirname = path.resolve();
 
-app.use(express.json({ limit: "5mb" })); // to parse req.body
-// limit shouldn't be too high to prevent DOS
-app.use(express.urlencoded({ extended: true })); // to parse form data(urlencoded)
+app.use(express.json({ limit: "5mb" }));
+app.use(express.urlencoded({ extended: true }));
 
 app.use(cookieParser());
 
--- middleware/ProtectRoute.js
+++ middleware/ProtectRoute.js
@@ -3,7 +3,7 @@ import jwt from "jsonwebtoken";
 
 export const protectRoute = async (req, res, next) => {
   try {
-    const token = req.cookies.jwt;
+    const token = req.cookies.access_token_x;
     if (!token) {
       return res.status(401).json({ error: "Unauthorized: No Token Provided" });
     }
